d9a432464be9da24072dbea6fa4aa59a9cb4c7c6
Murari <murarikumar42@gmail.com>
2026-03-17 21:13:22 +0530

    update time to 48 hrs for cooldown period

--- src/main/java/com/careerpolitics/scraper/config/WebConfig.java
+++ src/main/java/com/careerpolitics/scraper/config/WebConfig.java
@@ -23,7 +23,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOriginPatterns(corsAllowedOriginPatterns.toArray(new String[0]))
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
